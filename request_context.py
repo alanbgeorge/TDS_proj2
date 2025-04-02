af8c89e39957945b4b4fdfa8c9bea411d070cc22
alanbgeorge <124446007+alanbgeorge@users.noreply.github.com>
2025-04-02 20:26:11 +0530

    Delete request_context.py

--- request_context.py
+++
@@ -1,6 +0,0 @@
-
-from contextvars import ContextVar
-from fastapi import Request
-
-# Create a context variable for storing the request
-current_request_var: ContextVar[Request] = ContextVar("current_request")
