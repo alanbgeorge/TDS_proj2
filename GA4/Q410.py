61a834f690f365e2245ea0a8be2944e44f8b4273
alanbgeorge <124446007+alanbgeorge@users.noreply.github.com>
2025-04-02 20:22:44 +0530

    Delete GA4 directory

--- GA4/Q410.py
+++
@@ -1,8 +0,0 @@
-import os, json
-
-def execute(question: str, parameter):
-        print(f"File Name: {os.path.basename(__file__)[0]}")
-        return {
-            "GA_No": {os.path.basename(__file__)},
-            "parameters": parameter
-        }
