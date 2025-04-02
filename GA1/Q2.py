251419d4ff1e6f2baccf8a616461b3911f8b0045
alanbgeorge <124446007+alanbgeorge@users.noreply.github.com>
2025-04-02 20:21:32 +0530

    Delete GA1 directory

--- GA1/Q2.py
+++
@@ -1,9 +0,0 @@
-import os, json, requests
-
-def execute(question: str, parameter):
-    # URL and parameters
-    url = parameter["url"]
-    params = {"email": parameter["email"]}
-    # Make the GET request
-    response = requests.get(url, params=params)
-    return response.text
